5488c8d0ab2717091c81a3a634b7da712c8335a5
Andrew Grizhenkov <andrew.grizhenkov@hmcts.net>
2026-07-01 19:50:41 +0100

    EXUI-4915 Simplify media viewer start page

--- src/app/start-page.component.ts
+++ src/app/start-page.component.ts
@@ -4,20 +4,14 @@ import { Component } from '@angular/core';
   selector: 'app-start-page',
   template: `
     <div class="govuk-width-container">
-      <a routerLink="/dm-store" class="govuk-tag govuk-tag--grey" style="float: right">DM-Store</a>
       <main class="govuk-main-wrapper">
         <div class="govuk-grid-row">
           <div class="govuk-grid-column-two-thirds">
-            <table class="govuk-table">
-              <caption class="govuk-table__caption govuk-!-padding-left-2">Media Viewer</caption>
-              <tbody class="govuk-table__body">
-                <tr class="govuk-table__row">
-                  <td class="govuk-table__cell">
-                    <a class="govuk-!-padding-left-2" routerLink="/media-viewer">Media Viewer</a>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+            <h1 class="govuk-heading-l">Media Viewer</h1>
+            <ul class="govuk-list">
+              <li><a routerLink="/media-viewer" class="govuk-link">Media Viewer</a></li>
+              <li><a routerLink="/dm-store" class="govuk-link">DM Store</a></li>
+            </ul>
           </div>
         </div>
       </main>
