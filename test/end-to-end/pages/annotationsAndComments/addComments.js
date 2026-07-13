9034c65199c5ad86f558ccc5e068c561b2fcb8bd
Andrew Grizhenkov <andrew.grizhenkov@hmcts.net>
2026-07-13 15:51:10 +0100

    EXUI-4885 Scope comment save to active panel

--- test/end-to-end/pages/annotationsAndComments/addComments.js
+++ test/end-to-end/pages/annotationsAndComments/addComments.js
@@ -9,8 +9,9 @@ module.exports = async function (commentText) {
   await I.click(commonConfig.highLightTextCount);
   await I.waitForElement(commonConfig.commentPopup);
   await I.retry(2).click(commonConfig.commentPopup);
-  await I.waitForElement(commonConfig.firstCommentXp);  
+  await I.waitForVisible(commonConfig.firstCommentXp);
   await I.fillField(commonConfig.firstCommentXp, commentText);
+  await I.scrollTo(commonConfig.saveButton);
   await I.waitForClickable(commonConfig.saveButton);
   await I.retry(3).click(commonConfig.saveButton);
   await I.waitNumberOfVisibleElements(commonConfig.commentsCount, comments + 1);
