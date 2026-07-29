a973e347e47840bfa6fa144e3d28a1a3aeb20f91
Olu <142989683+olusegz07@users.noreply.github.com>
2026-07-29 09:35:11 +0100

    Update addCommentAndRotate.js

--- test/end-to-end/pages/annotationsAndComments/addCommentAndRotate.js
+++ test/end-to-end/pages/annotationsAndComments/addCommentAndRotate.js
@@ -4,10 +4,15 @@ const testConfig = require("../../../config");
 
 module.exports = async function () {
   const I = this;
-  
+
   await I.retry(2).click(commonConfig.rotateRightBtn);
+
+  // Give the PDF viewer time to rerender after rotation
+  await I.wait(2)
 
   await I.addComments(commonConfig.firstComment1);
 
   await I.retry(3).click(commonConfig.rotateLeftBtn);
+
+  await I.wait(2);
 }
